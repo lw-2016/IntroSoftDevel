4b3f944444b3e3aab84a2a7df0c6d6b990a2dd60
lukas <krieger.alg3rnop@gmail.com>
2016-10-16 20:54:50 +0100

    minor addition for test purposes

--- session2/session2_task_4.py
+++ session2/session2_task_4.py
@@ -1,2 +1,2 @@
-print("Hello World")
-print ("I am in my ISD class right now")
+print("Hello World")                                        #prints   Hello World 
+print ("I am in my ISD class right now")            #prints  I am in my ISD class right now
