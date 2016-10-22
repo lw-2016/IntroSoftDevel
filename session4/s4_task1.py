80b3d950adfb1f0dd9f166d4422484b32652a814
lukas <krieger.alg3rnop@gmail.com>
2016-10-22 16:10:49 +0100

    added int type conversion to input function and renamed file name

---
+++ session4/s4_task1.py
@@ -0,0 +1,14 @@
+##radius = input("Radius:")
+##x = 3.14
+##pi = x
+##area = pi * radius ** 2
+
+
+# 1.there was no - int type convertion which returns error when user attempts to input number.
+radius = int(input("Radius:"))
+x = 3.14
+pi = x
+area = pi * radius ** 2
+
+#2.added print statement
+print (area)
