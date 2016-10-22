81df43359e7b1982b80cd828d8507704107e8210
lukas <krieger.alg3rnop@gmail.com>
2016-10-22 16:18:19 +0100

    revert to basic version

--- session4/s4_task1.py
+++ session4/s4_task1.py
@@ -1,14 +1,5 @@
-##radius = input("Radius:")
-##x = 3.14
-##pi = x
-##area = pi * radius ** 2
-
-
-# 1.there was no - int type convertion which returns error when user attempts to input number.
-radius = int(input("Radius:"))
+radius = input("Radius:")
 x = 3.14
 pi = x
 area = pi * radius ** 2
 
-#2.added print statement
-print (area)
